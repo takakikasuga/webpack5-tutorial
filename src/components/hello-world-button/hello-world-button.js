1d80ddcceb31a842661c76efd45034519abfa28c
takakikasuga <takaki.soccer1@gmail.com>
2021-10-10 12:09:42 +0900

    css-loaderでjsからcssに変換

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,11 +1,15 @@
-// const helloWorld = () => {
-//   console.log('Hello world!');
-// };
-
+import './hello-world-button.css';
 class HelloWorldButton {
   render() {
     const button = document.createElement('button');
     button.innerHTML = 'Hello World!';
+    button.classList.add('hello-world-buton');
+    button.onclick = () => {
+      const p = document.createElement('p');
+      p.innerHTML = 'Hello World!';
+      p.classList.add('hello-world-text');
+      body.appendChild(p);
+    };
     const body = document.querySelector('body');
     body.appendChild(button);
   }
